adc8e7552150cf40cbb15a90dc49374a504ca921
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-12-09 15:53:34 +0100

    Complete milestone_1

--- index.php
+++ index.php
@@ -106,7 +106,23 @@
     </header>
     <!-- /#site_header -->
     <main id="site_main">
-
+        <div class="container-fluid">
+            <div class="row gx-5 my-5 d-flex justify-content-center">
+                <?php foreach ($disks as $disk) :?>
+                    <div class="col-md-2 text-center my-3">
+                        <div class="card rounded-0 p-2">
+                            <img src="<?php echo $disk['poster'] ?>" alt="<?php echo $disk['author'] ?>" class="img-fluid" />
+                            <h3 class="title mt-3"><?php echo $disk['title'] ?></h3>
+                            <div class="text mt-2">
+                                <p><?php echo $disk['author'] ?></p>
+                                <p><?php echo $disk['year'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                
+                <?php endforeach; ?>
+            </div>
+        </div>
     </main>
     <!-- /#site_main -->      
             
